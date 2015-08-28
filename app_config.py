cd90c114f516ac87a85fa9515d5820774f7c12a7
louisstow <louisstow@gmail.com>
2015-08-28 13:31:43 +1000

    * Change config settings

--- app_config.py
+++ app_config.py
@@ -25,7 +25,7 @@ REPOSITORY_URL = 'git@github.com:nprapps/%s.git' % REPOSITORY_NAME
 REPOSITORY_ALT_URL = None # 'git@bitbucket.org:nprapps/%s.git' % REPOSITORY_NAME'
 
 # Path to the folder containing the graphics
-GRAPHICS_PATH = os.path.abspath('../graphics')
+GRAPHICS_PATH = os.path.abspath('./graphics')
 
 # Path to the graphic templates
 TEMPLATES_PATH = os.path.abspath('graphic_templates')
@@ -52,21 +52,6 @@ authomatic = Authomatic(authomatic_config, os.environ.get('AUTHOMATIC_SALT'))
 """
 DEPLOYMENT
 """
-PRODUCTION_S3_BUCKET = {
-    'bucket_name': 'apps.npr.org',
-    'region': 'us-east-1'
-}
-
-STAGING_S3_BUCKET = {
-    'bucket_name': 'stage-apps.npr.org',
-    'region': 'us-east-1'
-}
-
-ASSETS_S3_BUCKET = {
-    'bucket_name': 'assets.apps.npr.org',
-    'region': 'us-east-1'
-}
-
 DEFAULT_MAX_AGE = 20
 ASSETS_MAX_AGE = 300
 
@@ -75,47 +60,17 @@ ANALYTICS
 """
 
 GOOGLE_ANALYTICS = {
-    'ACCOUNT_ID': 'UA-5828686-75'
+    'ACCOUNT_ID': ''
 }
 
 # These variables will be set at runtime. See configure_targets() below
-S3_BUCKET = None
-S3_BASE_URL = ''
-S3_DEPLOY_URL = None
+FTP_URL = 'contentftp.abc.net.au'
+FTP_USER = os.environ.get('FTP_USER')
+FTP_PASS = os.environ.get('FTP_PASS')
+FTP_PATH = '/www/dat/news/interactives/graphics/'
 DEBUG = True
-
-def configure_targets(deployment_target):
-    """
-    Configure deployment targets. Abstracted so this can be
-    overriden for rendering before deployment.
-    """
-    global S3_BUCKET
-    global S3_BASE_URL
-    global S3_DEPLOY_URL
-    global DEBUG
-    global DEPLOYMENT_TARGET
-
-    if deployment_target == 'production':
-        S3_BUCKET = PRODUCTION_S3_BUCKET
-        S3_BASE_URL = 'http://%s/%s' % (S3_BUCKET['bucket_name'], PROJECT_SLUG)
-        S3_DEPLOY_URL = 's3://%s/%s' % (S3_BUCKET['bucket_name'], PROJECT_SLUG)
-        DEBUG = False
-    elif deployment_target == 'staging':
-        S3_BUCKET = STAGING_S3_BUCKET
-        S3_BASE_URL = 'http://%s/%s' % (S3_BUCKET['bucket_name'], PROJECT_SLUG)
-        S3_DEPLOY_URL = 's3://%s/%s' % (S3_BUCKET['bucket_name'], PROJECT_SLUG)
-        DEBUG = True
-    else:
-        S3_BUCKET = None
-        S3_BASE_URL = 'http://127.0.0.1:8000'
-        S3_DEPLOY_URL = None
-        DEBUG = True
-
-    DEPLOYMENT_TARGET = deployment_target
 
 """
 Run automated configuration
 """
 DEPLOYMENT_TARGET = os.environ.get('DEPLOYMENT_TARGET', None)
-
-configure_targets(DEPLOYMENT_TARGET)
