9dbc31ceb8d3c3bcede0a7626d141a84ca11a259
louisstow <louisstow@gmail.com>
2015-09-16 10:08:46 +1000

    * Customize bar fill

--- graphic_templates/bar_chart/js/graphic.js
+++ graphic_templates/bar_chart/js/graphic.js
@@ -1,12 +1,42 @@
 // Global vars
 var pymChild = null;
 var isMobile = false;
+var graphicData = null;
+var graphicConfig = null;
 
 /*
  * Initialize the graphic.
  */
 var onWindowLoaded = function() {
     if (Modernizr.svg) {
+        graphicConfig = GRAPHIC_CONFIG;
+        loadLocalData(GRAPHIC_DATA);
+        //loadCSV('data.csv')
+    } else {
+        pymChild = new pym.Child({});
+    }
+}
+
+/*
+ * Load graphic data from a local source.
+ */
+var loadLocalData = function(data) {
+    graphicData = data;
+
+    formatData();
+
+    pymChild = new pym.Child({
+        renderCallback: render
+    });
+}
+
+/*
+ * Load graphic data from a CSV.
+ */
+var loadCSV = function(url) {
+    d3.csv(GRAPHIC_DATA_URL, function(error, data) {
+        graphicData = data;
+
         formatData();
 
         pymChild = new pym.Child({
@@ -174,6 +204,9 @@ var renderBarChart = function(config) {
             .attr('height', barHeight)
             .attr('class', function(d, i) {
                 return 'bar-' + i + ' ' + classify(d[labelColumn]);
+            })
+            .attr('fill', function(d) {
+                return graphicConfig.barColor || "#17807E";
             });
 
     /*
