eece5445ef5801348b6d8cbde06cf7a557d6b364
louisstow <louisstow@gmail.com>
2015-09-25 15:50:44 +1000

    * Is date

--- graphic_templates/line_chart/js/graphic.js
+++ graphic_templates/line_chart/js/graphic.js
@@ -40,8 +40,10 @@ var onWindowLoaded = function() {
 var loadLocalData = function(data) {
     graphicData = data;
 
-    formatData();
-
+    if (graphicData[0].date) {
+        formatData();
+    }
+    
     pymChild = new pym.Child({
         renderCallback: render
     });
