d97929b410e0b6b933210aaf3de7efe3161b01a5
louisstow <louisstow@gmail.com>
2015-09-28 09:50:35 +1000

    * Fix range

--- graphic_templates/line_chart/js/graphic.js
+++ graphic_templates/line_chart/js/graphic.js
@@ -253,10 +253,11 @@ var renderLineChart = function(config) {
         };
 
         xScale = d3.scale.ordinal()
-        .rangeRoundBands([0, chartWidth], .1)
+        // .rangeRoundBands([0, chartWidth], .1)
         .domain(graphicData.map(function (d) {
             return d['x'];
-        }));
+        }))
+        .range([0, chartWidth]);
     }
 
     var min = d3.min(config['data'], function(d) {
