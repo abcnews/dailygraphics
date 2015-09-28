e367b20f14dedab7a0c0d88f21593c2aa56d3b83
louisstow <louisstow@gmail.com>
2015-09-28 11:14:05 +1000

    * Add x label

--- graphic_templates/line_chart/js/graphic.js
+++ graphic_templates/line_chart/js/graphic.js
@@ -437,6 +437,13 @@ var renderLineChart = function(config) {
 
                 return label;
             });
+
+    if (graphicConfig.xLabel) {
+        chartElement.append("text")
+            .attr("x", chartWidth / 2)
+            .attr("y", chartHeight)
+            .text(graphicConfig.xLabel);
+    }
 }
 
 /*
