ed1668c7266c246a3fb77453e25290371a9e11c0
Blake Butcher <blake.butcher@gmail.com>
2016-06-08 17:51:38 +1000

    Seperated grouped bar charts into multiple SVGs to allow wrapping group headings
    Temporarily removed highlight logic.
    Fixes #49.

--- graphic_templates/grouped_bar_chart/js/graphic.js
+++ graphic_templates/grouped_bar_chart/js/graphic.js
@@ -66,42 +66,21 @@ var renderGroupedBarChart = function () {
 
     var barHeight = parseInt(LABELS.barHeight || 30, 10);
     var barGap = parseInt(LABELS.barGap || 10, 10);
-    var groupGap = parseInt(LABELS.groupGap || 35, 10);
     var labelWidth = parseInt(LABELS.labelWidth || 85, 10);
     var labelMargin = parseInt(LABELS.labelMargin || 6, 10);
     var valueGap = parseInt(LABELS.valueGap || 6, 10);
-    var groupHeadingHeight = 20;
-    var groupHeight = (barHeight + barGap) * numGroupBars - barGap;
+    var groupHeight = (barHeight + barGap) * numGroupBars;
 
     var margins = {
         top: parseInt(LABELS.marginTop || 0, 10),
         right: parseInt(LABELS.marginRight || 30, 10),
-        bottom: parseInt(LABELS.marginBottom || 20, 10),
+        bottom: parseInt(LABELS.marginBottom || 0, 10),
         left: parseInt(LABELS.marginLeft || (labelWidth + labelMargin), 10),
     };
 
     // Clear existing graphic (for redraw)
     var containerElement = d3.select('#grouped-bar-chart');
     containerElement.html('');
-
-    /*
-     * Create the root SVG element.
-     */
-    var chartWrapper = containerElement.append('div')
-        .attr('class', 'graphic-wrapper');
-
-    // Calculate actual chart dimensions
-    var innerWidth = chartWrapper.node().getBoundingClientRect().width;
-    var chartWidth = innerWidth - margins.left - margins.right;
-    var chartHeight = (groupHeight + groupGap) * numGroups - groupGap;
-
-    var chartElement = chartWrapper.append('svg')
-        .attr({
-            width: chartWidth + margins.left + margins.right,
-            height: chartHeight + margins.top + margins.bottom,
-        })
-        .append('g')
-            .attr('transform', makeTranslate(margins.left, margins.top));
 
     /*
      * Create D3 scale objects.
@@ -132,6 +111,16 @@ var renderGroupedBarChart = function () {
         });
     }
 
+    /*
+     * Create the root SVG element.
+     */
+    var chartWrapper = containerElement.append('div')
+            .attr('class', 'graphic-wrapper');
+
+    // Calculate actual chart dimensions
+    var innerWidth = chartWrapper.node().getBoundingClientRect().width;
+    var chartWidth = innerWidth - margins.left - margins.right;
+
     var xScale = d3.scale.linear()
         .domain([minX, maxX])
         .range([0, chartWidth]);
@@ -149,152 +138,146 @@ var renderGroupedBarChart = function () {
             return getAccessibleColor(color);
         }));
 
-    /*
-     * Render bars to chart.
-     */
-    var barGroups = chartElement.selectAll('.bars')
-        .data(DATA)
-        .enter()
-        .append('g')
-            .attr('class', 'g bars')
-            .attr('transform', function (d, i) {
-                return makeTranslate(0, (groupHeight + groupGap) * i + groupHeadingHeight);
-            });
+    DATA.forEach(function (dat, ind) {
 
-    var bars = barGroups.selectAll('rect')
-        .data(function (d) {
-            return d.values;
-        })
-        .enter()
-        .append('rect')
-            .attr({
-                x: function (d) {
-                    if (d.amt >= 0) {
-                        return xScale(0);
-                    }
-
-                    return xScale(d.amt);
-                },
-
-                y: function (d, i) {
-                    return (barHeight + barGap) * i;
-                },
-
-                width: function (d) {
-                    return Math.abs(xScale(0) - xScale(d.amt));
-                },
-
-                height: barHeight,
-
-                fill: function (d) {
-                    return colorScale(d.label);
-                },
-            })
-            .attr('class', function (d) {
-                return 'y-' + classify(d.label);
-            });
-
-    barGroups.append('text')
-            .attr('y', -barGap)
+        var groupLabels = chartWrapper.append('div')
             .attr('class', 'group-label')
-            .text(function (d) {
-                return d.key;
-            });
+            .style('margin-left', margins.left + 'px')
+            .text(dat.key + ' is a long group');
 
-    var labelData = [];
-    DATA.forEach(function (d) {
-        d.values.forEach(function (e) {
+        var labelData = [];
+        dat.values.forEach(function (e) {
             labelData.push(e.label);
         });
+
+        /*
+         * Render bar labels.
+         */
+        chartWrapper.append('ul')
+            .attr('class', 'labels')
+            .style({
+                width: labelWidth + 'px',
+                top: margins.top + 'px',
+                left: 0,
+            })
+            .selectAll('li')
+            .data(labelData)
+            .enter()
+            .append('li')
+                .attr('class', function (d, i) {
+                    return classify(d);//.key);
+                })
+                .style({
+                    width: labelWidth + 'px',
+                    height: barHeight + 'px',
+                    left: 0,
+                    top: function (d, i) {
+                        var barIndex = i % numGroupBars;
+                        var barOffset = (barHeight + barGap) * barIndex;
+                        var top = barOffset;
+                        return top + 'px';
+                    },
+                })
+                .append('span')
+                    .text(function (d) {
+                        return d;//.key
+                    });
+
+        var chartElement = chartWrapper.append('svg')
+            .attr({
+                width: chartWidth + margins.left + margins.right,
+                height: groupHeight + margins.top + margins.bottom,
+            })
+            .append('g')
+                .attr('transform', makeTranslate(margins.left, margins.top));
+
+        /*
+         * Render bars to chart.
+         */
+        var bars = chartElement.selectAll('rect')
+            .data(dat.values)
+            .enter()
+            .append('rect')
+                .attr({
+                    x: function (d) {
+                        if (d.amt >= 0) {
+                            return xScale(0);
+                        }
+
+                        return xScale(d.amt);
+                    },
+
+                    y: function (d, i) {
+                        return (barHeight + barGap) * i;
+                    },
+
+                    width: function (d) {
+                        return Math.abs(xScale(0) - xScale(d.amt));
+                    },
+
+                    height: barHeight,
+
+                    fill: function (d) {
+                        return colorScale(d.label);
+                    },
+                })
+                .attr('class', function (d) {
+                    return 'y-' + classify(d.label);
+                });
+
+        /*
+         * Render bar values.
+         */
+        var values = chartElement.append('g')
+            .attr('class', 'value')
+            .selectAll('text')
+            .data(dat.values)
+            .enter()
+            .append('text')
+                .text(function (d) {
+                    return formattedNumber(
+                        d.amt,
+                        LABELS.prefixX,
+                        LABELS.suffixX,
+                        LABELS.maxDecimalPlaces
+                    );
+                })
+                .attr({
+                    x: function (d) {
+                        if (d.amt < 0) {
+                            return xScale(0);
+                        }
+
+                        return xScale(d.amt);
+                    },
+
+                    y: function (d, i) {
+                        return (barHeight + barGap) * i;
+                    },
+
+                    dx: valueGap,
+
+                    dy: (barHeight / 2),
+
+                    fill: function (d) {
+                        return accessibleColorScale(d.label);
+                    },
+
+                });
+
     });
 
     /*
-     * Render bar labels.
-     */
-    chartWrapper.append('ul')
-        .attr('class', 'labels')
-        .style({
-            width: labelWidth + 'px',
-            top: margins.top + 'px',
-            left: 0,
-        })
-        .selectAll('li')
-        .data(labelData)
-        .enter()
-        .append('li')
-            .attr('class', function (d, i) {
-                return classify(d);//.key);
-            })
-            .style({
-                width: labelWidth + 'px',
-                height: barHeight + 'px',
-                left: 0,
-                top: function (d, i) {
-                    var groupIndex = Math.floor(i / numGroupBars);
-                    var groupOffset = (groupHeight + groupGap) * groupIndex;
-                    var barIndex = i % numGroupBars;
-                    var barOffset = (barHeight + barGap) * barIndex;
-                    var top = groupOffset + groupHeadingHeight + barOffset;
-                    return top + 'px';
-                },
-            })
-            .append('span')
-                .text(function (d) {
-                    return d;//.key
-                });
-
-    /*
-     * Render bar values.
-     */
-    var values = barGroups.append('g')
-        .attr('class', 'value')
-        .selectAll('text')
-        .data(function (d) {
-            return d.values;
-        })
-        .enter()
-        .append('text')
-            .text(function (d) {
-                return formattedNumber(
-                    d.amt,
-                    LABELS.prefixX,
-                    LABELS.suffixX,
-                    LABELS.maxDecimalPlaces
-                );
-            })
-            .attr({
-                x: function (d) {
-                    if (d.amt < 0) {
-                        return xScale(0);
-                    }
-
-                    return xScale(d.amt);
-                },
-
-                y: function (d, i) {
-                    return (barHeight + barGap) * i;
-                },
-
-                dx: valueGap,
-
-                dy: (barHeight / 2),
-
-                fill: function (d) {
-                    return accessibleColorScale(d.label);
-                },
-
-            });
-
     if (LABELS.theme == 'highlight') {
         var highlightGroupIndex;
         var highlightBarIndex;
 
         chartWrapper.on('mousemove', function (e) {
             var posY = d3.mouse(chartWrapper.node())[1];
-            var groupOffset = groupHeight + groupGap;
+            var groupOffset = groupHeight;
             var groupIndex = Math.floor(posY / groupOffset);
             var relativeY = posY - groupIndex * groupOffset;
-            var barIndex = Math.floor((relativeY - groupHeadingHeight) / (barHeight + barGap));
+            var barIndex = Math.floor((relativeY - groupHeadingLineHeight) / (barHeight + barGap));
 
             if ((highlightGroupIndex === groupIndex && highlightBarIndex === barIndex)) {
                 return; // still over the same element
@@ -309,7 +292,7 @@ var renderGroupedBarChart = function () {
 
             values.classed('over', false);
 
-            if (relativeY < groupHeadingHeight) {
+            if (relativeY < groupHeadingLineHeight) {
                 return; // in the group heading
             }
 
@@ -321,6 +304,7 @@ var renderGroupedBarChart = function () {
                 .classed('over', true);
         });
     }
+    */
 
 };
 
