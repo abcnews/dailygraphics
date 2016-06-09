004712fe8150a502ecd1f055a3150f6d42d025d3
Blake Butcher <blake.butcher@gmail.com>
2016-06-09 12:33:47 +1000

    Improved label alignment at smaller barHeights

--- graphic_templates/grouped_bar_chart/js/graphic.js
+++ graphic_templates/grouped_bar_chart/js/graphic.js
@@ -168,6 +168,7 @@ var renderGroupedBarChart = function () {
                     return classify(d);//.key);
                 })
                 .style({
+                    'line-height': barHeight >= 20 ? 14 + 'px' : barHeight + 'px',
                     width: labelWidth + 'px',
                     height: barHeight + 'px',
                     left: 0,
