f46fc718b2ec67ec96a9929b3eb934b8508fb332
Blake Butcher <blake.butcher@gmail.com>
2016-06-10 11:10:44 +1000

    Shell script for building my test graphics

---
+++ build-test-graphics.sh
@@ -0,0 +1,18 @@
+# READYISH TO USE
+fab debug_deploy:blake-line-chart,template=line_chart
+fab debug_deploy:blake-line-chart-linear,template=line_chart
+fab debug_deploy:blake-dot-chart,template=dot_chart
+fab debug_deploy:blake-slopegraph,template=slopegraph
+fab debug_deploy:blake-bar-chart,template=bar_chart
+fab debug_deploy:blake-grouped-bar-chart,template=grouped_bar_chart
+fab debug_deploy:blake-stacked-bar-chart,template=stacked_bar_chart
+fab debug_deploy:blake-column-chart,template=column_chart
+fab debug_deploy:blake-stacked-column-chart,template=stacked_column_chart
+fab debug_deploy:blake-pie-chart,template=pie_chart
+
+# NOT SO MUCH
+fab debug_deploy:blake-table,template=table
+fab debug_deploy:blake-block-histogram,template=block_histogram
+fab debug_deploy:blake-state-grid-map,template=state_grid_map
+fab debug_deploy:blake-locator-map,template=locator_map
+fab debug_deploy:blake-graphic,template=graphic
