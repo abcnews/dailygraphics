e26187fdbbb2b801c2d71897b2a15c28249cea6b
Blake Butcher <blake.butcher@gmail.com>
2016-07-25 10:18:59 +1000

    Seperate graphic template for bubbleplot

---
+++ graphic_templates/bubbleplot/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1jTHohHF2s5EqcjtJ1cZ0noN8wFyGqsLbGw1pxmgp9yQ' # ABC copy
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
