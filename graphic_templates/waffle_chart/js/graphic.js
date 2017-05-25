4065d29c73d766a2607d9367b19b48603a683be2
Joshua Byrd <phocks@gmail.com>
2017-05-25 17:10:44 +1000

    Putting it back to a pie
    I need to build waffle chart from scratch because d3 has no built in
    chart for that.

--- graphic_templates/waffle_chart/js/graphic.js
+++ graphic_templates/waffle_chart/js/graphic.js
@@ -72,90 +72,81 @@ var renderWaffleChart = function () {
     var chartHeight = chartWidth;
 
     var chartElement = chartWrapper.append('svg')
-                                     .attr("width", 400)
-                                     .attr("height", 400);
- 
-    //Draw the Rectangle
-    var rectangle = chartElement.append("rect")
-                                .attr("x", 10)
-                                .attr("y", 10)
-                                .attr("width", 10)
-                                .attr("height", 10);
-    //     .attr({
-    //         width: chartWidth + margins.left + margins.right,
-    //         height: chartHeight + margins.top + margins.bottom,
-    //     })
-    //     .append('g')
-    //         .attr('transform', makeTranslate(margins.left, margins.top));
+        .attr({
+            width: chartWidth + margins.left + margins.right,
+            height: chartHeight + margins.top + margins.bottom,
+        })
+        .append('g')
+            .attr('transform', makeTranslate(margins.left, margins.top));
 
-    // var overlay = chartElement.append('rect')
-    //     .attr({
-    //         width: chartWidth,
-    //         height: chartHeight,
-    //         fill: 'transparent',
-    //     });
+    var overlay = chartElement.append('rect')
+        .attr({
+            width: chartWidth,
+            height: chartHeight,
+            fill: 'transparent',
+        });
 
-    // var colorList = colorArray(LABELS, MULTICOLORS);
-    // var colorScale = d3.scale.ordinal()
-    //     .range(colorList);
+    var colorList = colorArray(LABELS, MULTICOLORS);
+    var colorScale = d3.scale.ordinal()
+        .range(colorList);
 
-    // var radius = chartWidth / 2;
+    var radius = chartWidth / 2;
 
-    // var arc = d3.svg.arc()
-    //     .outerRadius(radius)
-    //     .innerRadius(0);
+    var arc = d3.svg.arc()
+        .outerRadius(radius)
+        .innerRadius(0);
 
-    // var pie = d3.layout.pie()
-    //     .sort(null)
-    //     .value(function (d) {
-    //         return d.amt;
-    //     });
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function (d) {
+            return d.amt;
+        });
 
-    // var g = chartElement.selectAll('.arc')
-    //     .data(pie(DATA))
-    //     .enter().append('g')
-    //         .attr('class', 'arc')
-    //         .attr('transform', makeTranslate(radius, radius));
+    var g = chartElement.selectAll('.arc')
+        .data(pie(DATA))
+        .enter().append('g')
+            .attr('class', 'arc')
+            .attr('transform', makeTranslate(radius, radius));
 
-    // g.append('path')
-    //     .attr('d', arc)
-    //     .style('fill', function (d, i) {
-    //         return colorScale(i);
-    //     });
+    g.append('path')
+        .attr('d', arc)
+        .style('fill', function (d, i) {
+            return colorScale(i);
+        });
 
-    // var text = g.append('text')
-    //     .attr('transform', function (d) {
-    //         return 'translate(' + arc.centroid(d) + ')';
-    //     })
-    //     .each(function (d) {
-    //         // Finds "\n" in text and splits it into tspans
-    //         var words = d.data.label.replace(/\\n/g, '\n').split('\n');
+    var text = g.append('text')
+        .attr('transform', function (d) {
+            return 'translate(' + arc.centroid(d) + ')';
+        })
+        .each(function (d) {
+            // Finds "\n" in text and splits it into tspans
+            var words = d.data.label.replace(/\\n/g, '\n').split('\n');
 
-    //         for (var i = 0; i < words.length; i++) {
-    //             var tspan = d3.select(this).append('tspan').text(words[i]);
-    //             if (i > 0) {
-    //                 tspan.attr({
-    //                     x: 0,
-    //                     dy: '1em',
-    //                 });
-    //             }
-    //         }
-    //     });
+            for (var i = 0; i < words.length; i++) {
+                var tspan = d3.select(this).append('tspan').text(words[i]);
+                if (i > 0) {
+                    tspan.attr({
+                        x: 0,
+                        dy: '1em',
+                    });
+                }
+            }
+        });
 
-    // text.append('tspan')
-    //     .attr('class', 'value')
-    //     .text(function (d) {
-    //         return formattedNumber(
-    //             d.data.amt,
-    //             LABELS.valuePrefix,
-    //             LABELS.valueSuffix,
-    //             LABELS.maxDecimalPlaces
-    //         );
-    //     })
-    //     .attr({
-    //         x: 0,
-    //         dy: '1.5em',
-    //     });
+    text.append('tspan')
+        .attr('class', 'value')
+        .text(function (d) {
+            return formattedNumber(
+                d.data.amt,
+                LABELS.valuePrefix,
+                LABELS.valueSuffix,
+                LABELS.maxDecimalPlaces
+            );
+        })
+        .attr({
+            x: 0,
+            dy: '1.5em',
+        });
 
 };
 
