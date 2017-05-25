d0962437676db5dc44b038117c2cfcdcb0cbf37e
Joshua Byrd <phocks@gmail.com>
2017-05-25 17:07:52 +1000

    Just testing

--- graphic_templates/waffle_chart/js/graphic.js
+++ graphic_templates/waffle_chart/js/graphic.js
@@ -34,7 +34,7 @@ var render = function (containerWidth) {
     isMobile = (containerWidth <= MOBILE_THRESHOLD);
 
     // Render the chart!
-    renderPieChart();
+    renderWaffleChart();
 
     // Update iframe
     if (pymChild) {
@@ -45,7 +45,7 @@ var render = function (containerWidth) {
 /*
  * Render a pie chart.
  */
-var renderPieChart = function () {
+var renderWaffleChart = function () {
     /*
      * Setup
      */
@@ -64,7 +64,7 @@ var renderPieChart = function () {
      * Create the root SVG element.
      */
     var chartWrapper = containerElement.append('div')
-        .attr('class', 'graphic-wrapper graph');
+        .attr('class', 'graphics-wrapper');
 
     // Calculate actual chart dimensions
     var innerWidth = chartWrapper.node().getBoundingClientRect().width;
@@ -72,83 +72,199 @@ var renderPieChart = function () {
     var chartHeight = chartWidth;
 
     var chartElement = chartWrapper.append('svg')
-        .attr({
-            width: chartWidth + margins.left + margins.right,
-            height: chartHeight + margins.top + margins.bottom,
-        })
-        .append('g')
-            .attr('transform', makeTranslate(margins.left, margins.top));
+                                     .attr("width", 400)
+                                     .attr("height", 400);
+ 
+    //Draw the Rectangle
+    var rectangle = chartElement.append("rect")
+                                .attr("x", 10)
+                                .attr("y", 10)
+                                .attr("width", 10)
+                                .attr("height", 10);
+    //     .attr({
+    //         width: chartWidth + margins.left + margins.right,
+    //         height: chartHeight + margins.top + margins.bottom,
+    //     })
+    //     .append('g')
+    //         .attr('transform', makeTranslate(margins.left, margins.top));
 
-    var overlay = chartElement.append('rect')
-        .attr({
-            width: chartWidth,
-            height: chartHeight,
-            fill: 'transparent',
-        });
+    // var overlay = chartElement.append('rect')
+    //     .attr({
+    //         width: chartWidth,
+    //         height: chartHeight,
+    //         fill: 'transparent',
+    //     });
 
-    var colorList = colorArray(LABELS, MULTICOLORS);
-    var colorScale = d3.scale.ordinal()
-        .range(colorList);
+    // var colorList = colorArray(LABELS, MULTICOLORS);
+    // var colorScale = d3.scale.ordinal()
+    //     .range(colorList);
 
-    var radius = chartWidth / 2;
+    // var radius = chartWidth / 2;
 
-    var arc = d3.svg.arc()
-        .outerRadius(radius)
-        .innerRadius(0);
+    // var arc = d3.svg.arc()
+    //     .outerRadius(radius)
+    //     .innerRadius(0);
 
-    var pie = d3.layout.pie()
-        .sort(null)
-        .value(function (d) {
-            return d.amt;
-        });
+    // var pie = d3.layout.pie()
+    //     .sort(null)
+    //     .value(function (d) {
+    //         return d.amt;
+    //     });
 
-    var g = chartElement.selectAll('.arc')
-        .data(pie(DATA))
-        .enter().append('g')
-            .attr('class', 'arc')
-            .attr('transform', makeTranslate(radius, radius));
+    // var g = chartElement.selectAll('.arc')
+    //     .data(pie(DATA))
+    //     .enter().append('g')
+    //         .attr('class', 'arc')
+    //         .attr('transform', makeTranslate(radius, radius));
 
-    g.append('path')
-        .attr('d', arc)
-        .style('fill', function (d, i) {
-            return colorScale(i);
-        });
+    // g.append('path')
+    //     .attr('d', arc)
+    //     .style('fill', function (d, i) {
+    //         return colorScale(i);
+    //     });
 
-    var text = g.append('text')
-        .attr('transform', function (d) {
-            return 'translate(' + arc.centroid(d) + ')';
-        })
-        .each(function (d) {
-            // Finds "\n" in text and splits it into tspans
-            var words = d.data.label.replace(/\\n/g, '\n').split('\n');
+    // var text = g.append('text')
+    //     .attr('transform', function (d) {
+    //         return 'translate(' + arc.centroid(d) + ')';
+    //     })
+    //     .each(function (d) {
+    //         // Finds "\n" in text and splits it into tspans
+    //         var words = d.data.label.replace(/\\n/g, '\n').split('\n');
 
-            for (var i = 0; i < words.length; i++) {
-                var tspan = d3.select(this).append('tspan').text(words[i]);
-                if (i > 0) {
-                    tspan.attr({
-                        x: 0,
-                        dy: '1em',
-                    });
-                }
-            }
-        });
+    //         for (var i = 0; i < words.length; i++) {
+    //             var tspan = d3.select(this).append('tspan').text(words[i]);
+    //             if (i > 0) {
+    //                 tspan.attr({
+    //                     x: 0,
+    //                     dy: '1em',
+    //                 });
+    //             }
+    //         }
+    //     });
 
-    text.append('tspan')
-        .attr('class', 'value')
-        .text(function (d) {
-            return formattedNumber(
-                d.data.amt,
-                LABELS.valuePrefix,
-                LABELS.valueSuffix,
-                LABELS.maxDecimalPlaces
-            );
-        })
-        .attr({
-            x: 0,
-            dy: '1.5em',
-        });
+    // text.append('tspan')
+    //     .attr('class', 'value')
+    //     .text(function (d) {
+    //         return formattedNumber(
+    //             d.data.amt,
+    //             LABELS.valuePrefix,
+    //             LABELS.valueSuffix,
+    //             LABELS.maxDecimalPlaces
+    //         );
+    //     })
+    //     .attr({
+    //         x: 0,
+    //         dy: '1.5em',
+    //     });
 
 };
+
+
+
+// var renderPieChart = function () {
+//     /*
+//      * Setup
+//      */
+//     var margins = {
+//         top: parseInt(LABELS.marginTop || 0, 10),
+//         right: parseInt(LABELS.marginRight || 15, 10),
+//         bottom: parseInt(LABELS.marginBottom || 20, 10),
+//         left: parseInt(LABELS.marginLeft || 15, 10),
+//     };
+
+//     // Clear existing graphic (for redraw)
+//     var containerElement = d3.select('#waffle-chart');
+//     containerElement.html('');
+
+//     /*
+//      * Create the root SVG element.
+//      */
+//     var chartWrapper = containerElement.append('div')
+//         .attr('class', 'graphics-wrapper');
+
+//     // Calculate actual chart dimensions
+//     var innerWidth = chartWrapper.node().getBoundingClientRect().width;
+//     var chartWidth = innerWidth - margins.left - margins.right;
+//     var chartHeight = chartWidth;
+
+//     var chartElement = chartWrapper.append('svg')
+//         .attr({
+//             width: chartWidth + margins.left + margins.right,
+//             height: chartHeight + margins.top + margins.bottom,
+//         })
+//         .append('g')
+//             .attr('transform', makeTranslate(margins.left, margins.top));
+
+//     var overlay = chartElement.append('rect')
+//         .attr({
+//             width: chartWidth,
+//             height: chartHeight,
+//             fill: 'transparent',
+//         });
+
+//     var colorList = colorArray(LABELS, MULTICOLORS);
+//     var colorScale = d3.scale.ordinal()
+//         .range(colorList);
+
+//     var radius = chartWidth / 2;
+
+//     var arc = d3.svg.arc()
+//         .outerRadius(radius)
+//         .innerRadius(0);
+
+//     var pie = d3.layout.pie()
+//         .sort(null)
+//         .value(function (d) {
+//             return d.amt;
+//         });
+
+//     var g = chartElement.selectAll('.arc')
+//         .data(pie(DATA))
+//         .enter().append('g')
+//             .attr('class', 'arc')
+//             .attr('transform', makeTranslate(radius, radius));
+
+//     g.append('path')
+//         .attr('d', arc)
+//         .style('fill', function (d, i) {
+//             return colorScale(i);
+//         });
+
+//     var text = g.append('text')
+//         .attr('transform', function (d) {
+//             return 'translate(' + arc.centroid(d) + ')';
+//         })
+//         .each(function (d) {
+//             // Finds "\n" in text and splits it into tspans
+//             var words = d.data.label.replace(/\\n/g, '\n').split('\n');
+
+//             for (var i = 0; i < words.length; i++) {
+//                 var tspan = d3.select(this).append('tspan').text(words[i]);
+//                 if (i > 0) {
+//                     tspan.attr({
+//                         x: 0,
+//                         dy: '1em',
+//                     });
+//                 }
+//             }
+//         });
+
+//     text.append('tspan')
+//         .attr('class', 'value')
+//         .text(function (d) {
+//             return formattedNumber(
+//                 d.data.amt,
+//                 LABELS.valuePrefix,
+//                 LABELS.valueSuffix,
+//                 LABELS.maxDecimalPlaces
+//             );
+//         })
+//         .attr({
+//             x: 0,
+//             dy: '1.5em',
+//         });
+
+// };
 
 /*
  * Initially load the graphic
